46b479d78072e85d0ca2ded124647b87b7c51cf3
Cataclypsme <38730890+Cataclypsme@users.noreply.github.com>
2021-09-24 18:17:27 +0100

    Program Built in 2014
    I built this Java application for the Statistical Institute of Niger in 2014.

---
+++ Demographie/src/demographie/Demographie.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demographie;
+
+/**
+ *
+ * @author utilisateur
+ */
+public class Demographie {
+public static void main(String[] args) {
+      java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new ddemographie().setVisible(true);
+                 
+            }
+        });
+}
+}
